0be6a5f8d13c05351c91eec879dbb938480eed05
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-03-15 17:12:09 +0600

    add DATABASE_URL

--- src/services/auth/registerUser.ts
+++ src/services/auth/registerUser.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-
-
-
+import { zodValidator } from "@/lib/zodValidator";
+import { signUpSchema } from "@/zod/user.validation";
+import { loginUser } from "./loginUser";
 
 export const registerUser = async (_currentState: any, formData: FormData): Promise<any> => {
     try {
@@ -10,10 +10,35 @@ export const registerUser = async (_currentState: any, formData: FormData): Prom
             name: formData.get("name"),
             email: formData.get("email"),
             password: formData.get("password"),
+            confirmPassword: formData.get("confirmPassword")
         };
 
         console.log("Formdata --- ", payload);
+        if (zodValidator(payload, signUpSchema).success === false) {
+            return zodValidator(payload, signUpSchema);
+        }
+        const validatedPayload: any = zodValidator(payload, signUpSchema).data;
+        console.log("validated payloaddddddd : ", validatedPayload)
+        const registerData = {
+            password: validatedPayload.password,
+            name: validatedPayload.name,
+            email: validatedPayload.email,
+        }
+        const newFormData = new FormData();
+        newFormData.append("data", JSON.stringify(registerData));
 
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/create-customer`, {
+            method: "POST",
+            body: newFormData, // FormData
+        });
+        const result = await res.json();
+        console.log(result, res);
+        
+        if (result.success) {
+            await loginUser(_currentState, formData);
+        }
+
+        return result;
     } catch (error: any) {
         if (error?.digest?.startWith('NEXT_REDIRECT')) {
             throw error;
